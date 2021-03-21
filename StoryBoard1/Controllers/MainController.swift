513a3ca46780754bb5e3bb555ad02239e0396096
dkjohn <dkjohn0825@gmail.com>
2021-03-21 11:48:47 +0900

    first commit

---
+++ StoryBoard1/Controllers/MainController.swift
@@ -0,0 +1,21 @@
+//
+//  MainController.swift
+//  StoryBoard1
+//
+//  Created by 최현준 on 2021/03/21.
+//
+
+import UIKit
+
+class MainController:UIViewController{
+    //MARK : LIFE CYCLE FUNCITONS
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ViewDidLoad")
+    }
+    
+}
+
+
